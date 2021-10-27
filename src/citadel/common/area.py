fd485541b0cd7d748b88e7d04d6e077873554115
ADAM MAGNESON <jamagneson@gmail.com>
2021-10-26 22:26:06 -0600

    Added type hinting and string dunder to Area

--- src/citadel/common/area.py
+++ src/citadel/common/area.py
@@ -1,11 +1,14 @@
-"""Common class for all of the various areas (stages) of the game."""
+"""Common class for all of the various areas (stages)."""
 
 class Area:
 
-    def __init__(self, name, terrain=None, description=None):
+    def __init__(self, name: str, terrain: str = None, description: str = None):
         self.name = name
         self.terrain = terrain or "Desert"
         self.description = description or "!!! Missing description !!!"
+
+    def __str__(self):
+        return self.name
 
     def __add__(self, add_desc):
         self.description += "\n" + add_desc
