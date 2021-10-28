797f0615f9b1f0433ed1fd6b5f3096f18dd18a0a
ADAM MAGNESON <jamagneson@gmail.com>
2021-10-28 15:55:38 -0600

    Added all weapons to file

--- src/citadel/common/weapons.py
+++ src/citadel/common/weapons.py
@@ -1,4 +1,6 @@
 """Common class for all Weapons"""
+import random
+
 
 class Weapon:
 
@@ -9,3 +11,39 @@ class Weapon:
 
     def __str__(self):
         return self.name
+
+
+class ShortSword(Weapon):
+
+    def __init__(self, name="Short Sword"):
+        super().__init__(name, attack_power=random.randint(1,5))
+
+
+class LongSword(Weapon):
+
+    def __init__(self, name="Long Sword", level=5):
+        level = random.randint(level-2, level+2)
+        attack_power = level + random.randint(-2, 2)
+        super().__init__(name, level=level, attack_power=attack_power)
+
+
+class TwoHandedSword(Weapon):
+
+    def __init__(self, name="Two-handed Sword", level=10):
+        level = random.randint(level-3, level+3)
+        attack_power = level + random.randint(-3, 3)
+        super().__init__(name, level=level, attack_power=attack_power)
+
+
+class Claymore(Weapon):
+
+    def __init__(self, name="Claymore", level=15):
+        level = random.randint(level, 20)
+        attack_power = random.randint(level, 20)
+        super().__init__(name, level=level, attack_power=attack_power)
+
+
+class DragonsBane(Weapon):
+
+    def __init__(self, name="Dragons' Bane Sword", level=20, attack_power=20):
+        super().__init__(name, level=level, attack_power=attack_power)
