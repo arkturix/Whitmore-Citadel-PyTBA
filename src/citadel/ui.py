a5125378ff2eaedb623644ea5a7dbf65a42b3245
ADAM MAGNESON <jamagneson@gmail.com>
2021-10-29 11:53:57 -0600

    Created UI file

---
+++ src/citadel/ui.py
@@ -0,0 +1,19 @@
+"""User Interface"""
+
+
+class CitadelUI:
+    
+    def __init__(self, screen_size="800x600"):
+        self.screen_size = screen_size
+
+    def display_description(self, obj):
+        pass
+
+    def wipe_input(self):
+        pass
+    
+    def wipe_description(self):
+        pass
+
+    def display_art(self, obj):
+        pass
